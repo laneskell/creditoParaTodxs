f75df6f9eb2e1cb9ece602f7bd985d897c77cae1
Kethreen Lanes <77758983+laneskell@users.noreply.github.com>
2021-07-07 11:43:18 -0300

    Delete myPokemon directory

--- myPokemon/myPokemon/APIService.swift
+++
@@ -1,8 +0,0 @@
-//
-//  APIService.swift
-//  myPokemon
-//
-//  Created by Kell Lanes on 06/07/21.
-//
-
-import Foundation
--- myPokemon/myPokemon/TypePokemon.swift
+++
@@ -1,8 +0,0 @@
-//
-//  TypePokemon.swift
-//  myPokemon
-//
-//  Created by Kell Lanes on 06/07/21.
-//
-
-import Foundation
